5c4e21923be144b994e86f768e69ff8ac237d837
CHINNATHAMBI, KARTHICK (kc3503) <karthick.chinnathambi@intl.att.com>
2019-07-19 12:20:05 +0530

    Added Dice, Use VectorDrawable support library

--- 04-hellokotlin/app/src/main/java/com/quickplay/vstb/hellokotlin/MainActivity.kt
+++ 04-hellokotlin/app/src/main/java/com/quickplay/vstb/hellokotlin/MainActivity.kt
@@ -3,14 +3,14 @@ package com.quickplay.vstb.hellokotlin
 import android.content.Context
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.random.Random;
 class MainActivity : AppCompatActivity() {
 
-    val numberGen = Random(932342)
-
+    private val numberGen = Random(932342)
+    private lateinit var diceImage: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,14 +22,24 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
             rollDice()
         }
+        diceImage = findViewById(R.id.roll_result)
     }
 
     private fun rollDice() {
-        val rollText = findViewById<TextView>(R.id.roll_msg)
-        toast(getString(R.string.dice_rolled));
-        rollText.text = "Rolled Number is ${numberGen.nextInt(6)+1}"
+        //toast(getString(R.string.dice_rolled));
+        val drawableResourceID = when(numberGen.nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+        diceImage.setImageResource(drawableResourceID)
     }
 
+    @SuppressWarnings("unused")
     private fun Context.toast(message: String, duration: Int = Toast.LENGTH_SHORT) {
         Toast.makeText(this, message, duration).show()
     }
